9a6aeeedd6a5025b5e54c3f0e03e741b044b00c7
Andrew Johnston <arjohnston3@gmail.com>
2020-02-19 10:43:25 -0800

    update SW & install redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import registerServiceWorker from './registerServiceWorker'
+import * as serviceWorker from './serviceWorker'
+import store from './store'
+import { Provider } from 'react-redux'
 
 import Login from './pages/Login'
 import Register from './pages/Register'
@@ -12,20 +14,25 @@ import 'normalize.css'
 import './index.css'
 
 ReactDOM.render(
-  <Router>
-    <div>
-      <Switch>
-        <Route exact path='/' component={Dashboard} />
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
-        <Route render={function () {
-          return <Error />
-        }}
-        />
-      </Switch>
-    </div>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/' component={Dashboard} />
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+          <Route render={function () {
+            return <Error />
+          }}
+          />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 )
 
-registerServiceWorker()
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register()
