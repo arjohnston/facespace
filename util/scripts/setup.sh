79b19e158a2b686518f5e508c5018d9c2fda69a6
Andrew Johnston <arjohnston3@gmail.com>
2020-02-20 09:14:00 -0800

    add comments for creating scripts

---
+++ util/scripts/setup.sh
@@ -0,0 +1,9 @@
+# Check to see if we have required dependencies. If not, install them:
+# - Node JS
+# - NPM
+# - PM2
+# - MongoDB
+# - Git CLI
+
+# Configure MongoDB
+# - Create a DB file, set up permissions if needed
--- util/setup.sh
+++
@@ -1,1 +0,0 @@
-# Convert the .js file into sh / bat
